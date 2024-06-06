f265107e50e74d87e86f537a83c07ad0b65e5e3d
Sunookimo <159039310+Sunookimo@users.noreply.github.com>
2024-06-06 10:46:17 -0500

    Update Vehiculo.java

--- src/main/java/parcial2/Vehiculo.java
+++ src/main/java/parcial2/Vehiculo.java
@@ -1,5 +1,47 @@
 package parcial2;
 
 public class Vehiculo {
-    // TODO: Tu código va aquí
+    private String marca;
+    private String modelo;
+    private int año;
+
+    //Todos los métodos tienen complejidad constante O(1)
+    public Vehiculo(String marca, String modelo, int año) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+
+    public Vehiculo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public String imprimirInformacion(){
+        String a = this.marca + this.modelo + this.año;
+        return a;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAnio(int año) {
+        this.año = año;
+    }
 }
