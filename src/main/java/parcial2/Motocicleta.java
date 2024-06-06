f54730591cb5bc0a389a5d4a6e14fc25b4083cee
Sunookimo <159039310+Sunookimo@users.noreply.github.com>
2024-06-06 10:39:22 -0500

    Update Motocicleta.java

--- src/main/java/parcial2/Motocicleta.java
+++ src/main/java/parcial2/Motocicleta.java
@@ -1,5 +1,30 @@
 package parcial2;
 
-public class Motocicleta {
-    // TODO: Tu código va aquí
+public class Motocicleta extends Vehiculo{
+    private TipoMotocicleta tipoMotocicleta;
+    private TipoCombustible combustible;
+    
+    //Complejidad constante O(1)
+    public Motocicleta(String marca, String modelo, int año) {
+        super(marca, modelo, año);
+    }
+
+    public Motocicleta() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    //Complejidad constante O(1)
+    @Override
+    public String imprimirInformacion() {
+        return super.imprimirInformacion() + ", " + this.tipoMotocicleta + ", combustion a " + this.combustible;
+    }
+    
+    //Complejidad constante O(1) para los métodos setters & getters
+    public TipoMotocicleta getTipoMotocicleta() {
+        return tipoMotocicleta;
+    }
+
+    public void setTipoMotocicleta(TipoMotocicleta tipoMotocicleta) {
+        this.tipoMotocicleta = tipoMotocicleta;
+    }
 }
